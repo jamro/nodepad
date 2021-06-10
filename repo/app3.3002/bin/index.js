f6c144f559fab79869b066fda30cc64745afaf09
jamro <jamro@jamro.net>
2021-06-10 20:23:54 +0200

    chore: remove repo folder

--- repo/app3.3002/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const http = require('http');
-
-console.log('Starting application jmrlab (port: 3002)...');
-
-http.createServer(function (request, response) {
-  console.log(`Request ${request.method} ${request.url}`);
-  response.end('<h1>Hello from jmrlab!</h1>New content is here', 'utf-8');
-}).listen(3002);
--- repo/jmrlab.8084/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const http = require('http');
-
-console.log('Starting application jmrlab (port: 8084)...');
-
-http.createServer(function (request, response) {
-  console.log(`Request ${request.method} ${request.url}`);
-  response.end('Hello from jmrlab!', 'utf-8');
-}).listen(8084);
