4c3629b42d4bff67c94bf6f391ed6636256028b6
jamro <jamro@jamro.net>
2021-06-10 20:49:19 +0200

    chore: clean up

--- repo/asdf.3332/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-      const http = require('http');
-
-      console.log('Starting application asdf (port: 3332)...');
-
-      http.createServer(function (request, response) {
-        console.log(`Request ${request.method} ${request.url}`);
-        response.end('Hello from asdf!', 'utf-8');
-      }).listen(3332);
